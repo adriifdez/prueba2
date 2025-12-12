17516d068d8d0dbcb5fbb4096b3e5f7c72b3fb23
Carlosfdezzz <cf283180@gmail.com>
2025-12-12 11:54:35 +0100

    modificacion del archivo

--- practica22/solucion.java
+++ practica22/solucion.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class solucion {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Este es mi cambio");
 		// 
 		String descripcionProducto;
 		
