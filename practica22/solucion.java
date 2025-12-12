b3e1f106206fbeba5f54d7e6b87765d9e6fa9734
adriifdez <fdezadri22@gmail.com>
2025-12-12 11:00:00 +0100

    Adición de archivos de programación

---
+++ practica22/solucion.java
@@ -0,0 +1,40 @@
+package tema2.practica22;
+
+import java.util.Scanner;
+
+public class solucion {
+
+	public static void main(String[] args) {
+		// 
+		String descripcionProducto;
+		
+		double precioEnEuros;
+		int cantidad;
+		boolean enviado;
+		final double iva=4;
+		double totalBruto, importeIva, totalGeneral;
+		
+		System.out.println("Introduce la descripción del producto: ");
+		Scanner teclado = new Scanner(System.in);
+		descripcionProducto= teclado.next();
+		System.out.println("Introduce la cantidad que quiere: ");
+		cantidad=teclado.nextInt();
+		System.out.println("Precio en euros");
+		precioEnEuros=teclado.nextDouble();
+		System.out.println("Enviado (sí/no): ");
+		enviado=teclado.nextBoolean();
+		
+		totalBruto=cantidad*precioEnEuros;
+		importeIva= totalBruto+(iva/100);
+		totalGeneral=totalBruto+importeIva;
+		
+		System.out.printf("%-20s %-7s %8s %6s %9s %7s %13s","Descripción Enviado Cantidad Precio Imp_Bruto Imp_iva Total_General");
+		System.out.print("-------------------------------------------------------------------");
+		System.out.printf("%-20s %-7s % %8d %6.2f %9.2f %7.2f %13.2f",descripcionProducto,enviado, cantidad, precioEnEuros,totalBruto,importeIva,totalGeneral);
+
+		
+		
+
+	}
+
+}
